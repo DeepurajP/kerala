0f535c6f6acba3c5270702f0291324a216f3bc79
Deepuraj <panengaldeepuraj@gmail.com>
2024-02-07 17:57:12 +0530

    adding again

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('emil');
@@ -19,4 +20,27 @@ const validateInput = () => {
     const passwordValue = password.value.trim();
     const password2lValue = password2.value.trim();
 
+=======
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('emil');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInput();
+
+    });
+
+const validateInput = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2lValue = password2.value.trim();
+
+>>>>>>> 241d2f932408c52ba00efcbde02757564a0d2152
 }
